f9af4e5ad369e79a858fcfa545cc75321a1a8c16
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-01-01 19:25:17 +0530

    added hosting

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import "@material-tailwind/react/tailwind.css";
 import "../styles/globals.css";
 
+// aws configuration
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports';
+
+Amplify.configure({
+  ...awsmobile, ssr: true
+});
+
 import Header from "../components/Header";
 import Head from "next/head";
 function MyApp({ Component, pageProps }) {
